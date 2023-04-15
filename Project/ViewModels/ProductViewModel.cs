a54249a13ccb63eca300ea8aa1cf4e249dde43f5
Mihai-Nicolae Dulgheru <dulgherumihai19@stud.ase.ro>
2023-04-15 14:15:13 +0300

    Add Create Product Button.

--- Project/ViewModels/ProductViewModel.cs
+++ Project/ViewModels/ProductViewModel.cs
@@ -1,17 +1,24 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Project.Data;
 using Project.Models;
 using Project.Services;
+using System.Windows.Input;
 
 namespace Project.ViewModels
 {
     internal class ProductViewModel : ObservableObject, IProductViewModel
     {
         private ProductInformation _productInformation;
+        private readonly IProductDatabase _productDatabase;
         private readonly IRestService _restService;
+        public ICommand AddProductCommand { get; }
 
         public ProductViewModel()
         {
+            _productDatabase = new ProductDatabase();
             _restService = new RestService();
+            AddProductCommand = new AsyncRelayCommand(AddProductAsync);
         }
 
         public async void ApplyQueryAttributes(IDictionary<string, object> query)
@@ -35,6 +42,12 @@ namespace Project.ViewModels
                     OnPropertyChanged();
                 }
             }
+        }
+
+        private async Task AddProductAsync()
+        {
+            _ = await _productDatabase.CreateProductAsync(new Product(_productInformation.Id, _productInformation.Title, _productInformation.Image, _productInformation.ImageType));
+            await Shell.Current.GoToAsync("..");
         }
     }
 }
