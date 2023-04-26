665ac6bb55fd2f876954eda63375f69d3a72b929
Mihai-Nicolae Dulgheru <dulgherumihai19@stud.ase.ro>
2023-04-26 23:50:42 +0300

    Add Recipe.

--- Project/Models/ProductInformation.cs
+++ Project/Models/ProductInformation.cs
@@ -15,7 +15,7 @@
         public string[] ImportantBadges { get; set; }
         public int IngredientCount { get; set; }
         public string IngredientList { get; set; }
-        public Ingredient[] Ingredients { get; set; }
+        //public Ingredient[] Ingredients { get; set; }
         public int Likes { get; set; }
         public Nutrition Nutrition { get; set; }
         public int Price { get; set; }
@@ -41,7 +41,7 @@
             ImportantBadges = importantBadges ?? throw new ArgumentNullException(nameof(importantBadges));
             IngredientCount = ingredientCount;
             IngredientList = ingredientList ?? throw new ArgumentNullException(nameof(ingredientList));
-            Ingredients = ingredients ?? throw new ArgumentNullException(nameof(ingredients));
+            //Ingredients = ingredients ?? throw new ArgumentNullException(nameof(ingredients));
             Likes = likes;
             Nutrition = nutrition ?? throw new ArgumentNullException(nameof(nutrition));
             Price = price;
--- Project/Utilities/Constants.cs
+++ Project/Utilities/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public static readonly string APIKey = "";
+        public static readonly string APIKey = "2d69666e28a64375b4507639e94c2d49";
         public static readonly string DatabaseFilename = "Database.db3";
         public static readonly SQLite.SQLiteOpenFlags Flags =
         // open the database in read/write mode
