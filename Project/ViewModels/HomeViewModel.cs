110558ee230b565d26e6c22a0395c6560a1fcf7b
Mihai-Nicolae Dulgheru <dulgherumihai19@stud.ase.ro>
2023-04-29 23:08:23 +0300

    Add RandomFoodTrivia.

--- Project/ViewModels/HomeViewModel.cs
+++ Project/ViewModels/HomeViewModel.cs
@@ -1,21 +1,48 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Project.Services;
 using Project.Views;
 using System.Windows.Input;
 
 namespace Project.ViewModels
 {
-    internal class HomeViewModel : IHomeViewModel
+    internal class HomeViewModel : ObservableObject, IHomeViewModel
     {
+        private readonly IRestService _restService;
+        private string _randomFoodTrivia;
         public ICommand GoToSearchProductPageCommand { get; }
 
         public HomeViewModel()
         {
+            _restService = new RestService();
             GoToSearchProductPageCommand = new AsyncRelayCommand(GoToSearchProductPageAsync);
+        }
+
+        public string RandomFoodTrivia
+        {
+            get => _randomFoodTrivia;
+            set
+            {
+                if (_randomFoodTrivia != value)
+                {
+                    _randomFoodTrivia = value;
+                    OnPropertyChanged();
+                }
+            }
         }
 
         private async Task GoToSearchProductPageAsync()
         {
             await Shell.Current.GoToAsync(nameof(IngredientsPage));
+        }
+
+        public async Task GetRandomFoodTriviaAsync()
+        {
+            string randomFoodTrivia = await _restService.RandomFoodTriviaAsync();
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                RandomFoodTrivia = randomFoodTrivia;
+            });
         }
     }
 }
