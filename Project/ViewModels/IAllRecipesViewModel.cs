08aaeb0a01a1998e39a43e35ad2d8d865e36a7d9
Ioana Colceag <ioana.kol1101@gmail.com>
2023-05-15 22:34:27 +0300

    Search Recipes.

--- Project/ViewModels/IAllRecipesViewModel.cs
+++ Project/ViewModels/IAllRecipesViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Project.ViewModels
 {
@@ -11,5 +12,6 @@ namespace Project.ViewModels
     {
         string Label { get; }
         IList<Recipe> Recipes { get; set; }
+        ICommand PerformSearchRecipe { get; }
     }
 }
