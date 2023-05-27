7087c60c54ae662bfa1cadb45068f32dea7cf0b2
Mihai-Nicolae Dulgheru <dulgherumihai19@stud.ase.ro>
2023-05-27 15:42:19 +0300

    Remove Extra Exports.

--- Project/Views/MobileAllRecipesPage.xaml.cs
+++ Project/Views/MobileAllRecipesPage.xaml.cs
@@ -2,10 +2,10 @@ namespace Project.Views;
 
 public partial class MobileAllRecipesPage : ContentPage
 {
-	public MobileAllRecipesPage()
-	{
-		InitializeComponent();
-	}
+    public MobileAllRecipesPage()
+    {
+        InitializeComponent();
+    }
 
     private void OnCollectionViewSelectionChanged(object sender, SelectionChangedEventArgs e)
     {
